31a836631ac950fe54c877742fa1afef2d2c71fe
Muhammad Zahid <muhammadali775@gmail.com>
2021-06-10 01:10:29 -0400

    Completed Assignmnet

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+class Dog
+    attr_reader :name
+    attr_accessor :breed
+    
+    
+        def initialize(name, breed= "Mutt")
+            @name = name
+            @breed = breed
+
+
+        end
+    
+
+
+end
+
